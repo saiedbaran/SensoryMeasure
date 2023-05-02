1a66d8dcca90dbfa57abb81c2eccc4c61651e079
Saeed Safikhani <s.safikhani@outlook.com>
2023-05-02 10:19:24 +0200

    PCG map

---
+++ Source/VRCharacter/Private/Gameplay/Coin.cpp
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Gameplay/Coin.h"
+
+
+// Sets default values
+ACoin::ACoin()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CoinMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CoinMesh"));
+	RootComponent = CoinMesh;
+	CoinMesh->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	CoinMesh->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
+	CoinMesh->SetGenerateOverlapEvents(true);
+}
+
+// Called when the game starts or when spawned
+void ACoin::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ACoin::RotateAroundAxis(float DeltaTime)
+{
+	CoinMesh->AddWorldRotation(RotationDirection * RotationSpeed * DeltaTime);
+}
+
+// Called every frame
+void ACoin::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if(bIsRotating)
+	{
+		RotateAroundAxis(DeltaTime);
+	}
+}
+
---
+++ Source/VRCharacter/Public/Gameplay/Coin.h
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Coin.generated.h"
+
+class UNiagaraSystem;
+
+UCLASS()
+class VRCHARACTER_API ACoin : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ACoin();
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	void RotateAroundAxis(float DeltaTime);
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+	UStaticMeshComponent* CoinMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Coin")
+	UNiagaraSystem* CoinEffect;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Coin")
+	float RotationSpeed = 10.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Coin")
+	FRotator RotationDirection = FRotator(0, 1, 0);
+
+protected:
+	bool bIsRotating = true;
+
+};
